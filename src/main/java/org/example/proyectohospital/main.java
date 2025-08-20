ae28437db05c9b1998a36420809505873ca6bf34
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2025-08-20 16:48:26 -0600

    Creando Main para el modelo

---
+++ src/main/java/org/example/proyectohospital/main.java
@@ -0,0 +1,8 @@
+package org.example.proyectohospital;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, mundo desde Java!");
+        System.out.println("Main para probar cada una de las funcionales del Modelo.");
+    }
+}
