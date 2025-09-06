7eca7ea8be8fc582cda38d229ae67f096c62ac76
sebas-rodriguezh <sebastian.rodriguez.hernandez@est.una.ac.cr>
2025-09-06 15:26:37 -0600

    Cambios

--- src/main/java/org/example/proyectohospital/Controller/TabEntregaFarmaceutaController.java
+++ src/main/java/org/example/proyectohospital/Controller/TabEntregaFarmaceutaController.java
@@ -2,15 +2,21 @@ package org.example.proyectohospital.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import org.example.proyectohospital.Modelo.Receta;
 
-public class TabEntregaFarmaceutaController {
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+public class TabEntregaFarmaceutaController implements Initializable {
     @FXML private TextArea txtDetallesEntregaFarmaceuta;
     @FXML private Button btnEntregarRecetaFarmaceuta;
-    @FXML private TableColumn colFechaRetiroEntrega;
-    @FXML private TableColumn colPacienteEntrega;
-    @FXML private TableColumn colIdEntrega;
-    @FXML private TableView tableTabEntregaFarmaceuta;
+    @FXML private TableColumn <Receta, LocalDate> colFechaRetiroEntrega;
+    @FXML private TableColumn <Receta, String> colPacienteEntrega;
+    @FXML private TableColumn <Receta,String> colIdEntrega;
+    @FXML private TableView <Receta> tableTabEntregaFarmaceuta;
     @FXML private Button btnBuscarTabEntregaFarmaceuta;
     @FXML private TextField txtIdNombreEntregaFarmaceuta;
 
@@ -24,5 +30,10 @@ public class TabEntregaFarmaceutaController {
 
     @FXML
     public void buscarRecetaParaEntrega(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
